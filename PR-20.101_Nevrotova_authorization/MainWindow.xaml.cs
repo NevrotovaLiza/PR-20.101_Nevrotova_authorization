7d9ced6b40eda1ac5085b4a78d79cbe676d70ebf
NevrotovaLiza <99070843+NevrotovaLiza@users.noreply.github.com>
2023-02-20 18:52:22 +0700

    Add files via upload

---
+++ PR-20.101_Nevrotova_authorization/MainWindow.xaml.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace PR_20._101_Nevrotova_authorization
+{
+    /// <summary>
+    /// Логика взаимодействия для MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void btnEnter_Click(object sender, RoutedEventArgs e)
+        {
+            string login = txtLogin.Text;
+            string password = txtPassword.Text;
+
+            KR_NevrotovaEntities bd = new KR_NevrotovaEntities();
+            var authorization = bd.User;
+            var role = bd.Role;
+            var user = authorization.Where(x => x.Login == login && x.Password == password).FirstOrDefault();
+
+            if (user != null)
+            {
+                if (user.ID_Role == 1)
+                {
+                    MessageBox.Show("Добро пожаловать! Ваша роль клиент");
+                }
+                if (user.ID_Role == 2)
+                {
+                    MessageBox.Show("Добро пожаловать! Ваша роль менеджер");
+                }
+                if (user.ID_Role == 3)
+                {
+                    MessageBox.Show("Добро пожаловать! Ваша роль администратор");
+                }
+            }
+            else
+            {
+                MessageBox.Show("Неверно введены логин или пароль!");
+            }
+        }
+    }
+}
